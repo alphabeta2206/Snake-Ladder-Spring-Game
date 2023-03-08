2ccc0085eb507da289d97cd4581a83785bf30dd0
pp-in-564 <abhinavshinow@gmail.com>
2023-03-08 13:05:42 +0530

    added payout multiplier and player exit

--- src/main/java/com/Task1/Task/gamelogic/GameLogic.java
+++ src/main/java/com/Task1/Task/gamelogic/GameLogic.java
@@ -4,7 +4,9 @@ import com.Task1.Task.dto.PlayerDTO;
 import lombok.Data;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public abstract class GameLogic {
@@ -13,10 +15,13 @@ public abstract class GameLogic {
     private Dice dice;
     private int totalMoves;
     private double pricePool;
+    private int playersWon;
+    private Map<PlayerDTO, Integer> winnerList;
 
     public GameLogic(List<PlayerDTO> players, double pricePool) {
         this.players = players;
         this.pricePool = pricePool;
+        this.winnerList = new HashMap<>();
         Collections.shuffle(this.players);
         dice = new Dice(6);
         playerTurn = 0;
@@ -24,7 +29,9 @@ public abstract class GameLogic {
 
     public abstract void rollDie();
     public abstract void calculatePayout();
-    public abstract void playerExit(PlayerDTO player);
+    public abstract double getPayoutMultiplier(int winNum);
+    public abstract void updateWinnerList(PlayerDTO player);
+    public void playerExit(PlayerDTO player) { players.remove(player); }
     public void updateGameState(PlayerDTO player){
         players.removeIf(p -> p.equals(player));
     }
--- src/main/java/com/Task1/Task/gamelogic/SNL.java
+++ src/main/java/com/Task1/Task/gamelogic/SNL.java
@@ -2,39 +2,40 @@ package com.Task1.Task.gamelogic;
 
 import com.Task1.Task.dto.PlayerDTO;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class SNL extends GameLogic{
+import static java.util.stream.Collectors.toMap;
+
+public class SNL extends GameLogic {
     private HashMap<Integer, Integer> snakes; // Key = head, value = tail
     private HashMap<Integer, Integer> ladders; // Key = bottom, value = top
     private HashMap<Integer, Integer> bonusLadder;
     private HashMap<Integer, Integer> bonusSnake;
-    private HashMap<PlayerDTO, Integer> winnerList;
+
     public SNL(List<PlayerDTO> players, double pricePool) {
         super(players, pricePool);
 
         Random random = new Random();
         snakes = new HashMap<>();
         ladders = new HashMap<>();
+
         // Generate Random Snakes
         Map<Integer, Integer> SNAKE_CONFIG = Map.of(6, 2, 8, 1, 5, 3, 2, 1, 4, 3);
-        for(Map.Entry<Integer, Integer> entry : SNAKE_CONFIG.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : SNAKE_CONFIG.entrySet()) {
             int key = entry.getKey();
             int value = entry.getValue();
-            int snakeHead = random.nextInt(8*(key-1)+1, 8*key+1);
-            int snakeTail = random.nextInt(8*(value-1)+1, 8*value+1);
+            int snakeHead = random.nextInt(8 * (key - 1) + 1, 8 * key + 1);
+            int snakeTail = random.nextInt(8 * (value - 1) + 1, 8 * value + 1);
             snakes.put(snakeHead, snakeTail);
         }
         // Generate Random Ladders
         Map<Integer, Integer> LADDER_CONFIG = Map.of(2, 6, 1, 7, 3, 8, 5, 6, 7, 8);
-        for(Map.Entry<Integer, Integer> entry : LADDER_CONFIG.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : LADDER_CONFIG.entrySet()) {
             int key = entry.getKey();
             int value = entry.getValue();
-            int ladderBottom = random.nextInt(8*(key-1)+1, 8*key+1);
-            int ladderTop = random.nextInt(8*(value-1)+1, 8*value+1);
+            int ladderBottom = random.nextInt(8 * (key - 1) + 1, 8 * key + 1);
+            int ladderTop = random.nextInt(8 * (value - 1) + 1, 8 * value + 1);
             ladders.put(ladderBottom, ladderTop);
         }
     }
@@ -43,31 +44,47 @@ public class SNL extends GameLogic{
     public void rollDie() {
         super.setTotalMoves(super.getTotalMoves() + 1);
         int dieValue = super.getDice().getValue();
-        int playerTurn = super.getPlayerTurn()%super.getPlayers().size();
+        int playerTurn = super.getPlayerTurn() % super.getPlayers().size();
         PlayerDTO player = super.getPlayers().get(playerTurn);
         int nextPosition = player.getPosition() + dieValue;
 
-        if(snakes.containsKey(nextPosition)){
+        if (snakes.containsKey(nextPosition)) {
             nextPosition = snakes.get(nextPosition);
-        }else if(ladders.containsKey(nextPosition)){
+        } else if (ladders.containsKey(nextPosition)) {
             nextPosition = ladders.get(nextPosition);
         }
         System.out.println(getPlayers());
         player.setPosition(nextPosition);
-        player.setMoves(player.getMoves()+1);
+        player.setMoves(player.getMoves() + 1);
         player.setPrevRoll(dieValue);
         super.getPlayers().set(playerTurn, player);
         System.out.println(getPlayers());
-
     }
 
     @Override
     public void calculatePayout() {
-        super.getPlayers().forEach(player -> player.setPayout(winnerList.get(player) * super.getPricePool()));
+        super.getPlayers().forEach(player -> player.setPayout(getPayoutMultiplier(super.getWinnerList().get(player)) * super.getPricePool()));
     }
 
     @Override
-    public void playerExit(PlayerDTO player) {
+    public double getPayoutMultiplier(int winNum) {
+        int playerCount = super.getPlayers().size();
+        if (playerCount == 2) {
+            if (winNum == 0) return 1;
+        } else if (playerCount == 3) {
+            if (winNum== 0) return 0.6;
+            else if (winNum == 1) return 0.4;
+        } else if (playerCount == 4) {
+            if (winNum == 0) return 0.5;
+            else if (winNum== 1) return 0.3;
+            else if (winNum == 2) return 0.2;
+        }
+        return 0;
+    }
 
+    @Override
+    public void updateWinnerList(PlayerDTO player) {
+        super.setPlayersWon(super.getPlayersWon() + 1);
+        super.getWinnerList().put(player, super.getPlayersWon());
     }
 }
