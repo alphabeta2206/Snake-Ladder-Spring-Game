5a67ac970b3ef33f12664959ffd508de1974e058
= <=>
2023-03-10 14:46:29 +0530

    updated

--- src/main/java/com/Task1/Task/controller/HomeController.java
+++ src/main/java/com/Task1/Task/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -53,6 +54,9 @@ public class HomeController {
         userService.saveUser(user);
         return "redirect:/login";
     }
+
+
+
 
     @GetMapping("/logout")
     public String logout() {
