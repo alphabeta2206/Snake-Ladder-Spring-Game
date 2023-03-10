341f5b5217dff63fc81102ba6b56f2ea5d2aa365
= <=>
2023-03-10 14:57:17 +0530

    controller for profile page added .

--- src/main/java/com/Task1/Task/controller/HomeController.java
+++ src/main/java/com/Task1/Task/controller/HomeController.java
@@ -5,6 +5,8 @@ import com.Task1.Task.model.Role;
 import com.Task1.Task.model.User;
 import com.Task1.Task.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -56,7 +58,10 @@ public class HomeController {
     }
 
 
-
+    @GetMapping("/profile")
+    public ResponseEntity<User> getProfile(Principal principal ) {
+        return new ResponseEntity<User>( userService.getByUsername( principal.getName() ) , HttpStatus.OK ) ;
+    }
 
     @GetMapping("/logout")
     public String logout() {
