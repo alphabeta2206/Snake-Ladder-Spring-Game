7be41b538cc544aeff46a5ffa437f97da145ed0e
= <=>
2023-03-12 16:51:57 +0530

    betamount deduction and payout kept in one controller and made it transactional/batch statement

--- src/main/java/com/Task1/Task/controller/GameController.java
+++ src/main/java/com/Task1/Task/controller/GameController.java
@@ -10,7 +10,10 @@ import com.Task1.Task.service.CurrencyService;
 import com.Task1.Task.service.GameService;
 import com.Task1.Task.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -98,36 +101,45 @@ public class GameController {
                 double multiplier = currencyService.getMultiplier(user.getCurrencyCode());
                 Bet bet = new Bet();
                 bet.setAmount(game.getBetAmount());
-                user.setWalletAmt(user.getWalletAmt() - (game.getBetAmount() * multiplier));
+                // It should be divide by multiplier because multiplier is to convert any currency to euro
+                // but here , converting euro to other currency
+                //user.setWalletAmt(user.getWalletAmt() - (game.getBetAmount() * multiplier));
                 bet.setPlaceTime(Timestamp.from(Instant.now()));
                 bet.setGameId(game.getId());
                 bet.setUserId(user.getId());
                 bets.put(user.getId(), bet);
-                userService.saveUser(user);
+                //userService.saveUser(user);
+                betService.saveBet( bet );
+
             });
             session.setAttribute("playerBets", bets);
+            session.setAttribute("betAmount" , game.getBetAmount() );
         }else throw new GameException("Minimum of Two Players required to start game");
         return "Game Started";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/cancelgame/{gid}")
-    public String deleteGame(@PathVariable long gid) {
+    public ResponseEntity<String > deleteGame(@PathVariable long gid) {
         Game game = gameService.getById(gid);
         Set<User> playerList = game.getPlayers();
         if (game.getGameStatus() == GameStatus.IN_PROGRESS || game.getGameStatus() == GameStatus.NEW) {
             gameService.saveGame(game);
             playerList.forEach(user -> {
                 Bet bet = betService.getByUserId(user.getId());
-                bet.setPayOff(0);
-                bet.setStatus('C');
-                betService.saveBet(bet);
+                // if game is cancelled before starting the game then bet will be null
+                // because bet is saved in startgame controller
+                if( bet != null ) {
+                    bet.setPayOff(0);
+                    bet.setStatus('C');
+                    betService.saveBet(bet);
+                }
             });
         }else throw new GameException("Game Already Cancelled");
         game.setGameStatus(GameStatus.COMPLETED);
         game.setCancelReason(CancelReason.USER_CANCELLED);
         gameService.saveGame(game);
-        return "Canncelled Game";
+        return new ResponseEntity<String>( "Game Cancelled" ,HttpStatus.OK ) ;
     }
 
     @GetMapping("/joingame/{gid}")
@@ -145,20 +157,24 @@ public class GameController {
 
     @RequestMapping("/endgame/{gid}/{payout}")
     @ResponseBody
+    @Transactional
     public String endGame(@PathVariable Long gid, @PathVariable double payout, HttpSession session) {
         Game game = gameService.getById(gid);
         Set<User> playerList = game.getPlayers();
         if (game.getGameStatus() == GameStatus.IN_PROGRESS) {
             gameService.saveGame(game);
             HashMap<Long, Bet> bets = (HashMap<Long, Bet>) session.getAttribute("playerBets");
+
             playerList.forEach(user -> {
                 Bet bet = bets.get(user.getId());
                 double multiplier = currencyService.getMultiplier(user.getCurrencyCode());
                 bet.setPayOff(payout);
-                user.setWalletAmt(user.getWalletAmt() + payout * multiplier);
+                user.setWalletAmt(user.getWalletAmt() - (Double)( session.getAttribute("betAmount") ) / multiplier );
+                user.setWalletAmt(user.getWalletAmt() + payout / multiplier);
                 bet.setSettleTime(Timestamp.from(Instant.now()));
                 bet.setStatus('S');
                 betService.saveBet(bet);
+                userService.saveUser( user );
             });
             game.setGameStatus(GameStatus.COMPLETED);
             gameService.saveGame(game);
