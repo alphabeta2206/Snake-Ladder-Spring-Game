889e7d70096c80eff55c232657f592b6cfa418ad
pp-in-564 <abhinavshinow@gmail.com>
2023-03-15 15:40:38 +0530

    Updated

--- src/main/java/com/spring/game/SpringGameApplication.java
+++ src/main/java/com/spring/game/SpringGameApplication.java
@@ -2,8 +2,10 @@ package com.spring.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringGameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGameApplication.class, args);
