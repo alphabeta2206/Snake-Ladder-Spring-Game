55d6bab7f2530bbc5bc19beb1e66d20d63a4ea8e
pp-in-564 <abhinavshinow@gmail.com>
2023-03-16 19:18:17 +0530

    Added Controller Functionality

--- src/main/java/com/Task1/Task/controller/HomeController.java
+++ src/main/java/com/Task1/Task/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.Task1.Task.exceptions.SessionActiveException;
 import com.Task1.Task.model.Role;
 import com.Task1.Task.model.User;
 import com.Task1.Task.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -30,24 +31,28 @@ public class HomeController {
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
+    @Operation(summary = "Home Page")
     @RequestMapping("/home")
     @ResponseBody
     public String home() {
         return "Welcome please login or register";
     }
 
+    @Operation(summary = "Displays Login Form")
     @GetMapping("/login")
     public String showLoginForm(HttpSession session) {
         if(session.getAttribute("sessionVar") == null) return "loginForm";
         return "redirect:/lobby";
     }
 
+    @Operation(summary = "Displays Register Form")
     @GetMapping("/register")
     public String showRegisterForm(Model model) {
         model.addAttribute("user", new User());
         return "registerForm";
     }
 
+    @Operation(summary = "Process User Register")
     @PostMapping("/process_register")
     public String processRegister(@RequestParam String username, @RequestParam String name, @RequestParam String password, @RequestParam Double walletAmt, @RequestParam String currencyCode) {
         User user = new User();
@@ -62,11 +67,13 @@ public class HomeController {
         return "redirect:/login";
     }
 
+    @Operation(summary = "Logged in User Data")
     @GetMapping("/profile")
     public ResponseEntity<User> getProfile(Principal principal ) {
         return new ResponseEntity<User>( userService.getByUsername( principal.getName() ) , HttpStatus.OK ) ;
     }
 
+    @Operation(summary = "Process User Logout")
     @GetMapping("/logout")
     public String logout() {
         return "logoutForm";
