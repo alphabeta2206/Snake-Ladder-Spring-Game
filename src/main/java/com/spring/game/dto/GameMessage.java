64323e629745398dc9da92890ebb2bb1b6d482e8
pp-in-564 <abhinavshinow@gmail.com>
2023-03-17 11:35:47 +0530

    Updated

--- src/main/java/com/spring/game/dto/GameMessage.java
+++ src/main/java/com/spring/game/dto/GameMessage.java
@@ -1,7 +1,6 @@
 package com.spring.game.dto;
 import com.spring.game.enums.MessageType;
 import lombok.Data;
-import org.springframework.messaging.simp.SimpMessageType;
 @Data
 public class GameMessage {
     private MessageType type;
